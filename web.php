0578709dec7f7110910ef36af2f60d8b7fc4dd6c
salsabila000 <86759929+salsabila000@users.noreply.github.com>
2021-11-10 17:02:35 +0700

    tugas kelompok semester 2

---
+++ web.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> Web Coffee</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style55.css">
+</head>
+<body>
+  <nav>
+    <div class="menu_bar">
+      <div class="logo">a Caffe</div>
+      <div class="slogan">just, take some rest</div>
+      <ul>
+        <li><a href="#HOME">HOME</a></li>
+        <li><a href="#OURVARIAN">OUR VARIAN</a></li>
+        <li><a href="#SPECIALOFFERS">SPECIAL OFFERS</a></li>
+        <li><a href="#REVIEWSS">REVIEWS</a></li>
+        <li><a href="#GETINTOUCH">GET IN TOUCH</a></li>
+      </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="isi">
+    <!--Home-->
+    <section id="HOME">
+      <div class="latar"></div>
+      <div class="homepage">
+        <h2>A good way to start a  day </h2>
+        <p>Special for coffee lovers !<br>only today you can get our special menu!<br>let's check it out </p>
+        <p class="today"><a href="#TODAYSSPECIAL">Today's Special</a></p>
+      </div>
+    </section>
+
+<!--Today's Special-->
+    <section id="TODAYSSPECIAL">   
+      <div class="title1">Today's Spesial</div>
+      <hr></hr>
+      <div class="coco">
+        <p>Hot Cocoa Malts And Oreo Cheesecake</p>
+      </div>
+      <div class="bs">
+        <p>it's rich and luscious, sweet and tangy, and those little bits of chocolate cookie pieces add such a decadent flavour, with creamy chocolate ice cream is blended together with decadent chocolate milk and malt powder until frothy and smooth from Hot Choco Matls. A must try dessert!</p></div>
+      <div class="gm1">
+        <img src="img/aa.jpeg" alt="oreo cheesecake">
+      </div>
+      <div class="grs"></div>
+      <div class="gm2">
+        <img src="https://kjandcompany.co/wp-content/uploads/2019/11/Hot-Cocoa-Malts-5.jpg" alt="Hot Choco Malt">
+      </div>
+      <div class="grs2"></div>
+    </section>
+
+<!--Our Variant-->
+    <section >
+      <div class="title2" id="OURVARIAN">OUR VARIANT</div>
+        <div class="box">
+          <div class="variant0">
+            <img src="img/xx.jpeg" class="image"alt="Hot cup of cofee" >
+            <div class="tengah">
+              <div class="desc">HOT</div>
+            </div>
+          </div>
+          <div class="variant1">
+            <img src="img/ee.jpeg" class="image1" alt="Cold Variant">
+            <div class="tengah1">
+              <div class="desc1">COLD</div>
+            </div>
+          </div>
+          <div class="variant2">
+            <img src="img/kue.jpeg"class="image2" alt="Dessert Variant" >
+            <div class="tengah2">
+              <div class="desc2">DESSERT</div>
+            </div>
+          </div>
+          <div class="variant3">
+            <img src="img/ZZ.jpeg" class="image3" alt= "Non Coffe" width="600" height="400">
+            <div class="tengah3">
+              <div class="desc3">NON COFFE</div>
+            </div>
+          </div>
+        </div>
+    </section>
+<!--Special Offer-->
+    <section>
+      <div class="title3" id="SPECIALOFFERS">SPECIAL OFFERS</div>
+      <div class="spesial1">
+        <div class="ce">
+          <img src="https://i.pinimg.com/originals/b2/bd/11/b2bd1160a1ec92b99199c5462699b1d9.jpg" alt="croisant">
+        </div>
+        <div class="xx">
+          <p><b> buy 1 <br> get 1</b></p>
+        </div>
+        <div class="df">
+          <p> FREE Cup Of Coffe*</p>
+        </div>
+        <div class="syarat">*Term and Condition
+          <span class="tooltiptext"> free 1 shoot expresso, dine in only</span>
+        </div>
+        <div class="decor"></div>
+      </div>
+    </section>
+
+<!--review-->
+    <section>
+      <div class="title4" id="REVIEWSS">Review</div>
+      <div class="reviews">
+        <div class="wa">
+          <p class="ll"><span>&#x2B50;</span></p>
+          <p class="ll1"><span>&#x2B50;</span></p>
+          <p class="ll2"><span>&#x2B50;</span></p>
+          <p class="jm"> 2 days ago</p> 
+          <div class="ulasan1">
+            <p>"Excellent environmment and <br> friendly service, thanks you.<br><b>-Caca</b></p>
+          </div>
+        </div>
+        <div class="wa1">
+          <p class="ll"><span>&#x2B50;</span></p>
+          <p class="ll1"><span>&#x2B50;</span></p>
+          <p class="ll2"><span>&#x2B50;</span></p>
+          <p class="ll3"><span>&#x2B50;</span></p>
+          <p class="ll4"><span>&#x2B50;</span></p>
+          <p class="jm"> 3 days ago</p> 
+          <div class="ulasan1">
+            <p> A unique coffee shop to study  and read  books. No loud noises which will help you a lot to concentrate on what you do."<br><b>-ayuni</b></p>
+          </div>
+        </div>
+        <div class="wa2">
+          <p class="ll"><span>&#x2B50;</span></p>
+          <p class="ll1"><span>&#x2B50;</span></p>
+          <p class="ll2"><span>&#x2B50;</span></p>
+          <p class="ll3"><span>&#x2B50;</span></p>
+          <p class="jm"> 3 days ago</p> 
+          <div class="ulasan1">
+            <p>The drinks and food in this Coffe Shop are very Good."<br><b>-Farhan</b></p>
+          </div>
+        </div>
+
+        <div class="wa3">
+          <p class="ll"><span>&#x2B50;</span></p>
+          <p class="ll1"><span>&#x2B50;</span></p>
+          <p class="ll2"><span>&#x2B50;</span></p>
+          <p class="ll3"><span>&#x2B50;</span></p>
+          <p class="ll4"><span>&#x2B50;</span></p>
+          <p class="jm"> 5 days ago</p> 
+          <div class="ulasan1">
+            <p>This coffee shop really fits with me, because from the taste really the coffee it's amazing."<br><b>-FATUR</b></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<!--About Us-->
+    <div class="aboutus">
+      <h2>About Us</h2>
+      <img src="img/store.jpg">
+      <p>a Caffee is a authentic Coffee Shop that has<br> 
+          been proudly serving the Jakarta Timur area<br> 
+          and beyond since 2020. Since then, our mission<br> 
+          has been to provide high-quality food and Coffee<br> 
+          for all those that wish to feel relaxed and enjoy<br></p>
+          <a href="#" class="button">Learn More</a> 
+    </div>
+  </div>
+  <footer>
+    <div class="content-about">
+      <h2 Id="GETINTOUCH">GET IN TOUCH</h2>
+      <p>Jl.Seira tanimbar, Ceger, Kec. Cipayung, Kota Jakarta Timur, DKI Jakarta,13850,Indonesia
+      <br>acaffe@gmail.com<br>0812-2342-345</p>
+      <img src="https://img.icons8.com/material-rounded/33/000000/facebook-new.png"/>
+      <img src="https://img.icons8.com/fluent-systems-filled/33/000000/twitter.png"/>
+      <img src="https://img.icons8.com/metro/33/000000/instagram-new.png"/>
+    </div>
+    <p>Copyright &copy 2021 | design by a caffe</p>
+  </footer>
+</body>
+</html>
